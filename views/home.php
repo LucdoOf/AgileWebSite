5c82ef99ac72b256356a47cc72e5d055645064d3
lucas <lucasg.18@hotmail.fr>
2020-03-01 17:19:41 +0100

    Main page progress

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@
         <header>
             <ul id="menu">
                 <li><a href="#" class="cw">Accueil</a></li>
-                <li><a href="#" class="cw">Notre engagement</a></li>
+                <li><a href="#" class="cw">Notre méthodes</a></li>
                 <li><a href="#" class="cw">Nos créations</a></li>
                 <li><a href="#" class="cw">Contact</a></li>
             </ul>
@@ -15,7 +15,7 @@
         <div id="main">
             <div id="home-wrapper">
                 <div id="centered-wrapper" class="wrapper absolute column align">
-                    <div class="wrapper row">
+                    <div id="computer-wrapper" class="wrapper row">
                         <div class="wrapper column">
                             <h1 class="cw">Agile-web.net</h1>
                             <h2 class="cw">Votre solution numérique</h2>
@@ -26,11 +26,57 @@
                                 Nous avons la <strong>solution</strong> qu'il vous faut !
                             </p>
                         </div>
-                        <img id="computer" alt="Computer" src="res/images/svg/computer.svg"/>
+                        <img id="computer-img" alt="Computer" src="res/images/svg/computer.svg"/>
                     </div>
                     <div id="discover" class="wrapper column cw justify align">
                         <a>Découvrez en plus !</a>
                         <img id="arrow" alt="Flèche" src="res/images/svg/arrow.svg"/>
+                    </div>
+                </div>
+            </div>
+            <div id="coworking-wrapper" class="row">
+                <img id="coworking-img" src="res/images/svg/coworking.svg" alt="Coworking"/>
+                <div id="paragraph-wrapper" class="column">
+                    <h2 class="dot">Votre projet mérite le meilleur</h2>
+                    <p class="cb">
+                        Nous développons votre projet en suivant une méthode agile<br>
+                        Nous vous accompagnons de la conception a la mise en ligne de votre site internet en passant par le <a target="_blank" href="https://fr.wikipedia.org/wiki/Optimisation_pour_les_moteurs_de_recherche">SEO</a><br>
+                        Le processus de développement se fait conjointement avec vous en communiquant régulièrement sur nos avancées<br>
+                    </p>
+                </div>
+            </div>
+            <div id="roadmap-wrapper">
+                <h2>Nous savons comment mener un projet</h2>
+                <div id="roadmap-grid">
+                    <div class="roadstep">
+                        <img src="res/images/svg/idea.svg" alt="Idée"/>
+                        <h4>1. L'idée</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
+                    </div>
+                    <div class="roadstep">
+                        <img src="res/images/svg/design.svg" alt="Design"/>
+                        <h4>2. Le design</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
+                    </div>
+                    <div class="roadstep">
+                        <img src="res/images/svg/conception.svg" alt="Conception"/>
+                        <h4>3. La conception</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
+                    </div>
+                    <div class="roadstep">
+                        <img src="res/images/svg/validation.svg" alt="Validation"/>
+                        <h4>4. La validation</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
+                    </div>
+                    <div class="roadstep">
+                        <img src="res/images/svg/retouches.svg" alt="Retouches"/>
+                        <h4>5. Les retouches</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
+                    </div>
+                    <div class="roadstep">
+                        <img src="res/images/svg/production.svg" alt="Mise en production"/>
+                        <h4>6. La mise en production</h4
+                        <p>Lorem ipsum dolor sit amet et mortum</p>
                     </div>
                 </div>
             </div>
