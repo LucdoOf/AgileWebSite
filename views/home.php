73468b5c31eea4e19bf0bca06b4a60b5f60d4d71
lucas <lucasg.18@hotmail.fr>
2020-03-04 22:06:59 +0100

    Fixed css issues

--- views/home.php
+++ views/home.php
@@ -2,6 +2,7 @@
     <head>
         <title>Agile-web.net</title>
         <link rel="stylesheet" href="res/stylesheets/css/main.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <header>
