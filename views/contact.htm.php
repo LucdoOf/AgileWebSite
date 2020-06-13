2bb05c96a0461cdadc6d1a7551f8a356687a10a7
lucas <lucasg.18@hotmail.fr>
2020-06-13 18:35:19 +0200

    Esthetic

--- views/contact.htm.php
+++ views/contact.htm.php
@@ -4,18 +4,14 @@
             <h2 class="section-title">Contactez-nous</h2>
             <p class="section-large-text">Remplissez le formulaire ci-dessous en indiquant votre mail, l'objet de votre demande ainsi que votre message. Que cela concerne une question sur notre démarche, une proposition ou une demande de devis, nous vous répondrons par mail dans les 12h.</p>
             <form id="contact-form" class="column justify align" method="post" action="<?= route("/contact" )?>">
-                <?php
-                    if(isset($message) && isset($success) && !is_null($message) && !is_null($success)){
-                        ?>
-                            <div id="submit-answer" class="<?= $success == true ? "success" : "error" ?>">
-                                <?= $message ?>
-                            </div>
-                        <?php
-                    }
-                ?>
                 <input type="email" placeholder="Mail" name="mail"/>
                 <input type="text" placeholder="Objet" name="subject"/>
                 <textarea placeholder="Votre demande" name="content"></textarea>
+                <?php if(isset($message) && isset($success) && !is_null($message) && !is_null($success)): ?>
+                    <div id="submit-answer" class="<?= $success == true ? "success" : "error" ?>">
+                        <?= $message ?>
+                    </div>
+                <?php endif; ?>
                 <div class="button-group">
                     <input type="submit" name="submit" class="button">
                 </div>
