fb80355ad97efeb524eb2d4f2ad3402314889c46
lucas <lucasg.18@hotmail.fr>
2020-06-19 11:48:58 +0200

    Added shipping section to agile-office page

--- views/agile-office.htm.php
+++ views/agile-office.htm.php
@@ -7,7 +7,7 @@
         <div class="content">
             <div class="header-section-text">
                 <h1 class="section-title">Passez au numérique</h1>
-                <p class="section-large-text">
+                <p class="section-text">
                     Vous avez toujours souhaité offrir un service en ligne tout en pouvant gérer vos commandes, vos produits, vos clients ou encore vos factures en un clic ? <br>
                     Choisissez notre solution Agile-Office sur mesure pour répondre à tous vos besoins
                 </p>
@@ -24,11 +24,19 @@
     <section class="gray padded">
         <div class="content column align justify">
             <h2 class="section-title center">Notre produit</h2>
-            <p class="section-large-text center">
+            <p class="section-text center">
                 Nous développons pour vous un site internet moderne et fonctionnel qui vous ressemble muni d'un service multiplateforme vous permettant
                 de gérer votre site en toute autonomie. <br>
             </p>
             <a class="button cta xl center" href="#" target="_blank">Essayez-le</a>
+        </div>
+    </section>
+    <section class="padded colored" id="shipping-section">
+        <div class="content column align justify fit">
+            <h2 class="section-title bold center">Ouvrez votre commerce au monde entier</h2>
+            <p class="section-large-text center bold">
+                Développez un système simple de livraison en France métropolitaine et dans le monde avec notre prise en charge des plus grands services de livraison mondiaux
+            </p>
         </div>
     </section>
 </div>
